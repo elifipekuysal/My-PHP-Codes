d6ff16e87c46e742a5b782fb270f1f1daee54178
Elif İpek Uysal <elifipekuysal@gmail.com>
2017-03-24 21:26:25 +0200

    Update publicandlocalip.php

--- publicandlocalip.php
+++ publicandlocalip.php
@@ -13,14 +13,32 @@
         <h1 style= " color: #404040; background-color: #ccccff">IP Adresiniz</h1>
         <fieldset>
             <?php
-                if (getenv('HTTP_FORWARDED_FOR')) {
+                $publicip= 0;
+            
+                if (getenv('HTTP_CLIENT_IP')) {
+                    $localip = getenv('REMOTE_ADDR');
+                    $publicip = getenv('HTTP_CLIENT_IP'); 
+                } else if(getenv('HTTP_X_FORWARDED_FOR')) {
+                    $localip = getenv('REMOTE_ADDR');
+                    $publicip = getenv('HTTP_X_FORWARDED_FOR');
+                } else if(getenv('HTTP_X_FORWARDED')) {
+                    $localip = getenv('REMOTE_ADDR');
+                    $publicip = getenv('HTTP_X_FORWARDED');
+                } else if(getenv('HTTP_FORWARDED_FOR')) {
                     $localip = getenv('REMOTE_ADDR');
                     $publicip = getenv('HTTP_FORWARDED_FOR');
-                    echo "<h3>Public IP'niz : $publicip </h3>" ;
-                    echo "<h3>Local IP'niz : $localip </h3>" ;
+                } else if(getenv('HTTP_FORWARDED')) {
+                    $localip = getenv('REMOTE_ADDR');
+                    $publicip = getenv('HTTP_FORWARDED');
                 } else {
                     $localip = getenv('REMOTE_ADDR');
-                    echo "<h3>Local IP'niz : $localip </h3>";
+                }
+                
+                if ($publicip != 0) {
+                    echo "<h3>Public IP'niz : $publicip </h3>" ;
+                    echo "<h3>Local IP'niz : $localip </h3>" ; 
+                } else {
+                    echo "<h3>Local IP'niz : $localip </h3>" ;                    
                 }
             ?>       
         </fieldset>
