77ee08ef69277c11940fc3d5034349ca8b2de749
Elif İpek Uysal <elifipekuysal@gmail.com>
2017-04-15 22:51:48 +0300

    Upload 2.php
    echo function with concatenation operator

---
+++ 2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+
+    <body>
+        <p>
+            <?php
+                echo "Hello".","." "."world"."!";    #The concatenation operator (.), glues these strings together
+            ?>
+        </p>
+    </body>
+</html>
