b5a462dd4de7d8a90296254ea171425201a0c4c4
Elif İpek Uysal <elifipekuysal@gmail.com>
2017-04-15 22:50:49 +0300

    Upload 1.php
    echo function

---
+++ 1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>My First PHP Code</title>
+    </head>
+
+    <body>
+        <p>
+                <!-- Start of a PHP script -->
+            <?php 
+                echo 'Hello World!'    # Here it prints the string "Hello World!"
+            ?>      <!-- End of this PHP script -->  
+        </p>
+    </body>
+
+</html>
