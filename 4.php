76264f2a33230a22003273e626bc8134f005a17f
Elif İpek Uysal <elifipekuysal@gmail.com>
2017-04-27 21:07:24 +0300

    Upload 4.php
    Variables

---
+++ 4.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <p>
+	    <?php
+	        $Var1 = "Hello World!";    # Var1 is a variable and stores a string ("Hello World!")
+	        echo $Var1;    # This line outputs the value that stored in Var1 variable.
+
+	        $Var2 = 2017;   # Var2 is a variable too and stores an integer (2017)
+            echo $Var2;   # This line outputs the value that stored in Var2 variable.
+	    ?>
+	</p>
+    </body>
+</html>
