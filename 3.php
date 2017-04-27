85ef76b746ba56752ac3a28c9b0301352ec9166f
Elif İpek Uysal <elifipekuysal@gmail.com>
2017-04-27 21:03:14 +0300

    Upload 3.php
    Simple mathematical operations

---
+++ 3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+
+    <body>
+        <p>
+            <?php
+                echo 2*9;   # Here PHP multiplies 2 and 9 and outputs 18.
+                echo 4+5;   # PHP sums 4 and 5 and outputs 9.
+                echo 10/2;  # PHP divides 10 with 2 and outputs 5.
+                echo 7-3;   # PHP subtracts 3 from 7 and outputs 4.
+            ?>
+        </p>
+    </body>
+</html>
